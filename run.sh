3666e4b70ced0df4704a468d8007575b0a1e50d6
Marek Bykowski <marek.bykowski@gmail.com>
2025-06-07 16:48:04 +0000

    Add cscope config
    my 'cs' alias goes to ~/.bashrc
    
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- run.sh
+++ run.sh
@@ -5,3 +5,15 @@ cp .tmux.conf .vimrc ~/
 git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
 
 test -d ~/.ssh && cp config ~/.ssh
+
+cat << 'EOF' >> ~/.bashrc
+cs_func() {
+        echo $PWD
+        find "$PWD" \( -name '*.[cshS]' -o -name '*.txt' -o -name '*.lds' \
+                        -o -name '*.pl' -o -name '*.rts' \) > "$PWD"/cscope.files
+        cd $PWD && cscope -b -R
+        CSCOPE_DB=${PWD}/cscope.out; export CSCOPE_DB
+        #cscope -b -q -k; SCOPE_DB=${PWD}/cscope.out; export CSCOPE_DB
+}
+alias cs=cs_func
+EOF
